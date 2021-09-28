2b1bd6283992f09938c85fde75c0bbcafc445407
shuvro baset <md.abdul.baset75@gmail.com>
2021-09-28 22:04:01 +0600

    django initialize

---
+++ pioneer_alpha_signup/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pioneer_alpha_signup project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pioneer_alpha_signup.settings')
+
+application = get_asgi_application()
